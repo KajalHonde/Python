a5885ad22ff78bb6f189fbe9501e6efdbea86368
Honde Kajal Santosh <hondekajal@gmail.com>
2026-04-01 12:15:39 -0700

    base_conversion

---
+++ DataTypes.py
@@ -0,0 +1,44 @@
+a=20
+b=21
+#Integer Data
+print(a+b)  #addition
+print(a-b)  #subs
+print(a*b)  #mult
+print(a/b)  #div
+print(a%b)  #modulus= gives remainder
+print(a//b) #floor divion =removes decimal part
+print(a**b) #power = 20 multiplied 21 times
+
+#Float Data 
+c=24.4
+d=5.5
+
+#add
+print(c+d)
+
+#round() function: takes to the nearest whole num
+print(round(d))
+
+#type() function: shows the dataType of the value
+print(type(c))
+
+#complex datatype shows imaginary part 
+#.real and .imag are the attributes
+z= 3+2j
+print(z.real)
+print(z.imag)
+
+#float data type
+a= 12.9
+type(a)
+
+# * used for repetation
+#rule is one should be intger
+s1 = "python is very easy"
+print(s1 * 2)
+
+# + used for contetnation 
+# rule is both data should have same data type
+s1 = "python is very easy"
+s2 = "hello" 
+print(s1 + s2)
---
+++ baseConversion.py
@@ -0,0 +1,24 @@
+#base conversion
+
+#1. bin to convert in binaryt form
+x = 0x1234AB
+b=  bin(x)
+print(b)
+
+#2. oct() converts any base into octal
+
+y = 0x123
+b=  oct(x)
+print(b)
+
+#3. hex) conerts any value into hexadecimal
+ 
+z = 0o1235
+b=  hex(x)
+print(b)
+
+#4. convert any number into decimal base
+
+w = 0o1235
+b=  int(x)
+print(b)
