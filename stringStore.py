6d6aee03e12bf011c6cef1edf0600d417eaab2ea
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-11 02:19:46 -0700

    stringStore

---
+++ stringStore.py
@@ -0,0 +1,22 @@
+#string
+
+s1 = 'geeta greeted "good morning"'
+s2= "geeta's voice is very sweet"
+s3="""geeta greeted, "good morning" and geets's voice is very sweet"""
+s4="geeta greeted,\"good evenng\"and geeta's voice is very sweet"
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+
+s1='''python is easy to learn'''
+print(s1)
+
+#index
+s11 = "python is easy"
+print(s11[-4])
+
+#slicing
+#string[start:end:step]
+print(s3[1:4])
+print(s3[3:5:2])
