8daf438e06f44d0397d41437d970a36f28af58f2
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-13 22:50:20 -0700

    input

---
+++ input.py
@@ -0,0 +1,40 @@
+#string
+'''
+x = input()
+print(x+" "+ "is easy")
+print(type(x))
+
+#will take string has input
+n1 = input()
+n2 = input()
+print(n1+n2)
+
+
+n1 = int(input())
+n2 = int(input())
+print(type(n1))
+print(n1+n2)
+
+m1=int(input())
+m2=int(input())
+m3=int(input())
+m4=int(input())
+m5=int(input())
+
+total =m1+m2+m3+m4+m5
+print(total)
+
+sum=(m1+m2+m3+m4+m5)/5
+print(sum)
+'''
+emp_num = int(input())
+emp_name = input()
+emp_salary = float(input())
+emp_Address = input()
+age = int(input())
+print("yes") if age >=18 else print("No")
+print(emp_num)
+print(emp_name)
+print(emp_salary)
+print(emp_Address)
+print(age)
