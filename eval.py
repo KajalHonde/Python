d15ba12374afcb6b764674979c139bafed4d962d
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-13 22:50:33 -0700

    eval

---
+++ eval.py
@@ -0,0 +1,34 @@
+'''
+x = eval(input())
+print(x)
+print(type(x))
+
+#inbuilt function
+
+
+cost1 = eval(input("enter your data"+" "))
+
+sales =eval(input())
+total = cost1+sales
+print(total)
+
+#swapping
+a = int(input())
+b= int(input())
+c = ""
+c = a
+a = b
+b =c
+print(a)
+print(b)
+
+
+# to remove decimal point
+a = float(input())
+b= int(a)
+print(b)
+
+'''
+from sys import *
+print(argv)
+print(type(argv))
