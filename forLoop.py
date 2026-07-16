a233468b7f6d98c14197d145a6f3aa148fe849a3
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-16 01:29:31 -0700

    forLoop

---
+++ forLoop.py
@@ -0,0 +1,31 @@
+'''
+s="python"
+for x in s:
+  print(x)
+  
+li =[10,20,30,40,50,60]
+for s in li:
+    print(s*s)
+    
+for i in range(1,11):
+    print(i)
+
+s=input("enter the string")
+n=-1
+i =0
+for x in s:
+   print(x,"postive index",i,"negative index",n,x)
+   i=i+1
+   n=n-1
+
+#to display numbers from 0 to 10
+for i in range(1,11):
+ print(i)
+ '''
+#prit sum of numbers in list
+l =[10,20,30,40,50]
+sum=0
+for s in l:
+    print(sum)
+    sum =sum+s
+   
