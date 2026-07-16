551b4c65107a044ae24bae6f47719566922ba424
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-16 01:29:04 -0700

    if

---
+++ if.py
@@ -0,0 +1,34 @@
+'''
+if else condtion
+# Find ot Max value
+a = int(input())
+b = int(input())
+if(a>=b):
+  print("a is greater")
+else:
+  print("b is grater")
+
+  #absolute value
+a =int(input())
+print(abs(a))  
+
+from sys import argv
+print(argv)
+if int(argv[1])>0:
+    print("argv is greater")
+else:
+    print("it's smaller")
+
+#if elif
+state=input("Enter your state")
+if(state == "kerala"):
+   print("capital:kottyam")
+elif(state == "karnataka"):
+  print("capital:banglre") 
+elif(state == "goa"):
+  print("capital:panji")
+else:
+  print("invalid state")      
+  '''
+  #if we want to execute a group of statements multiple times then we should go for iterative statements
+  
