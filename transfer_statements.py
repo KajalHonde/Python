0d075c924996c727e9ad73e1f6ab2340a0dcb579
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-18 19:20:21 -0700

    transfer_satements

--- transfer_statements.py
+++ transfer_statements.py
@@ -30,10 +30,42 @@ for item in cart:
         print("can't proceed")
         continue
     print(item)
-'''
+
 num=[10,20,0,5,30,0]
 for n in num:
     if n==0:
      print("num cannot be divided by 0")
      continue
     print("100/{}={}".format(n,100/n))
+    
+
+i=1
+while i<=10:
+ if i==5:
+  continue
+
+ print(i,end=" ")
+ i=i+1
+
+i = 1
+while i <= 10:
+    if i == 5:
+        continue
+
+    print(i, end=" ")
+    i = i + 1
+
+for x in range(1,50):
+    if x%9==0:
+        print(x)
+    else:
+        pass
+'''
+cart = [10,20,600,30,40,50]
+for item in cart:
+    if item>=500:
+        print("we cannot proceed")
+        break
+    print(item)
+else:
+    print("successfully done")
