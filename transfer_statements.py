d658110d848b824f49fe68e81ef4810a2430868f
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-18 18:01:54 -0700

    transfer_statement

---
+++ transfer_statements.py
@@ -0,0 +1,39 @@
+'''
+for i in range(10):
+    if i==5:
+        print(i)
+        break
+    print(i)
+
+cart=[10,20,30,40,50]
+for item in cart:
+    if item>500:
+        print("insurance required")
+        break
+    print(item)
+
+for i in range(10):
+    if i ==7:
+        print("insurance required")
+        continue
+    print(i)
+
+for i in range(10):
+    if i%2!=0:
+        continue
+    print(i)
+
+
+cart = [10,20,30,500,700,60]
+for item in cart:
+    if item>500:
+        print("can't proceed")
+        continue
+    print(item)
+'''
+num=[10,20,0,5,30,0]
+for n in num:
+    if n==0:
+     print("num cannot be divided by 0")
+     continue
+    print("100/{}={}".format(n,100/n))
