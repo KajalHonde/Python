18f68d2c88b93f409e02d90c6b60add6a8aa5692
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-21 00:12:57 -0700

    dictionary_dataType

---
+++ dict.py
@@ -0,0 +1,47 @@
+# empty dict
+'''
+d={}
+print(d)
+print(type(d))
+
+
+# B intialization
+d={1:"python",2:"java",3:"c++"}
+print(d)
+print(d[1])
+print(type(d))
+
+# dynamic dict
+d=eval(input("enter data:"))
+print(type(d))
+print(d)
+
+
+# dict()
+d=dict([(1,"tom"),(2,"jack")])
+print(type(d))
+print(d)
+
+# e
+d={"name":["kajal","komal","priya"],"age":[10,30,40]}
+print(d)
+print(type(d))
+
+# Adding values in dict
+d={1:"kajal"}
+d[2]="java"
+print(d)
+d[2]="DS"
+print(d)
+
+for x in d:
+    print(d)
+    
+'''
+# deletinng
+d={"name":["kajal","komal","priya"],"age":[10,30,40]}
+print(len(d))
+print(d)
+del d["name"]
+print(d)
+print(type(d))
