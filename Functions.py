827efeaedcb0bb5507cfc4c27a55cfdb517aef61
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-22 00:18:14 -0700

    Functions

---
+++ Functions.py
@@ -0,0 +1,37 @@
+'''
+print("Good evening")
+def show():
+    print("This is show")
+    return
+
+def display():
+    print("this is display function")
+    return
+
+
+display()
+show()
+display()
+print("end of an application")
+show()
+
+print("Good evening")
+def show():
+    print("hi")
+    display()
+    print("This is show")
+    
+
+def display():
+    print("this is display function")
+
+show()
+print("End of the application")
+'''
+def add():
+    n1=int(input("enter num1"))
+    n2=int(input("enter num2"))
+    n3=n1+n2
+    print("sum of num1 and num2 is",n3)
+    
+add()
