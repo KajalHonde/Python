c17380b12355da80ab7fbd3e6f21b60ed244a96d
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-23 14:11:28 -0700

    decorator

---
+++ decorator.py
@@ -0,0 +1,79 @@
+'''
+def decorator (func):
+    def inner(name):
+        if name=="guest":
+            print("Dear guest you need to register yourself")
+        else:
+            func(name)
+            
+    return inner
+            
+            
+@decorator
+def wish(name):
+    print("hello",name)
+    
+    
+wish("kajal")
+wish("guest")
+
+n1=90
+n22=0
+print(n1/n22)
+
+def decorator (func):
+    def inner(n1,n2):
+        if n2==0:
+            print("Number can't be divided")
+        else:
+            func(n1,n2)
+            
+    return inner
+
+@decorator
+def divide (n1,n2):
+    print(n1/n2)
+
+# print(divide(10,0))
+
+divide(10,0)
+divide(10,2)
+
+
+# using decorator without  calling it with "annotatio"
+def decorator (func):
+    def inner(name):
+        if name=="guest":
+            print("Dear guest you need to register yourself")
+        else:
+            func(name)
+            
+    return inner
+ 
+ 
+def wish(name):
+     print("hello",name)
+        
+dfunction=decorator(wish)
+dfunction("tom")
+            
+'''
+# decorator chaining
+def decor1(wish):
+    def inner(name):
+        print("first decor")
+        wish(name)
+    return inner
+
+def decor2(wish):
+    def inner(name):
+        print("second decor")
+        wish(name)
+    return inner
+
+@decor1
+@decor2
+def  wish(name):
+    print("hello",name)
+    
+wish("kajal")
