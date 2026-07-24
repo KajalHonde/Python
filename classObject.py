7bbaa181c261ed01469990c8826c10a10836ba01
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-24 11:57:11 -0700

    classObject

--- classObject.py
+++ classObject.py
@@ -1,3 +1,4 @@
+'''
 class Test():
     def show(self):
         print(self)
@@ -14,3 +15,30 @@ print("************************")
 t2=Test()
 t2.show()
 t2.display()
+'''
+
+# methods with parameters
+from math import*
+class Math:
+    def accept(self,n1,n2):
+        self.x=n1
+        self.y=n2
+    def add(self):
+        print("Addition",self.x+self.y)
+        
+    def sub(self):
+        return self.x-self.y
+    
+    def mult(self,x,y):
+        return x*y
+        
+m1=Math()
+n1=int(input("Enter N1"))
+n2=int(input("Enter N1"))
+
+m1.accept(n1,n2)
+m1.add()
+res=m1.sub()
+print(res)
+r=m1.mult(n1,n2)
+print(r)
