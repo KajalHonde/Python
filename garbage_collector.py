8a90a367a15d0418768ed9111f2e504bf4ff7c21
Honde Kajal Santosh <hondekajal@gmail.com>
2026-07-30 00:49:33 +0530

    garbage_collector

--- garbage_collector.py
+++ garbage_collector.py
@@ -1,3 +1,4 @@
+'''
 import gc
 
 print("Garbage collector is enabled",gc.isenabled())
@@ -7,3 +8,19 @@ print("Garbage collector is enabled",gc.isenabled())
 
 gc.disable()
 print("garbage collector is disable",gc.isenabled())
+'''
+
+import time
+class Test:
+    def __init__(self):
+        print("constructor")
+        
+    def __del__(self):
+        print("destructor invoked for clean up activity")
+        
+t1=Test()
+time.sleep(5)
+print("work done")
+t1=None
+time.sleep(10)
+print("end of application")
