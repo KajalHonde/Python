2bdf65566efc1c3a19e180d4a3a8cf7d50bf27b2
Honde Kajal Santosh <hondekajal@gmail.com>
2026-08-02 21:47:22 +0530

    Operator_overloading

--- Operator_overloading.py
+++ Operator_overloading.py
@@ -5,7 +5,15 @@ class Books:
     def __add__(self, other):
         print(self.pages)
         print(other.pages)
-        return self.pages+other.pages
+        pgs=self.pages+other.pages
+        return Books(pgs)
+    # def __mul__(self,other):
+    #     print(self.b1,other.b2)
+    #     return self.b1*self.b2
+    
 b1=Books(200)
 b2=Books(300)
-print(b1+b2)
+b3=Books(100)
+b4=b1+b2+b3
+print(b4.pages)
+# print(b1*b2)
