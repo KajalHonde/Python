16363cfddafbbb35f2ae59167262a9ab73c28179
Honde Kajal Santosh <hondekajal@gmail.com>
2026-08-03 22:23:22 +0530

    ExceptionHanding

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -21,7 +21,7 @@ except:
     print("divion not possible")
 
 print("End of application")
-'''
+
 
 n1=int(input("enter numerator"))
 n2=int(input("enter denominator"))
@@ -32,3 +32,48 @@ except(ZeroDivisionError,NameError,ValueError) as msg:
     print("Error",msg)
 except:
     print("Something  went wrong")
+    
+
+try:
+    n1=int(input())
+    n2=int(input())
+    n3=n1/n2
+    print(n3)
+# except ZeroDivisionError:
+#     print("It can't be divided by zero")
+except ArithmeticError:
+    print("Its Arithimetic error")
+except ValueError:
+    print("its value error")
+except ZeroDivisionError:
+    print("It can't be divided by zero")
+except:
+    print("some other error")
+print("End of the application")
+
+
+try:
+    n1=int(input())
+    n2=int(input())
+    n3=n1/n2
+    print(n3)
+except ZeroDivisionError:
+    print("It can't be divided by zero")
+    
+finally:
+    print("clean up activity")
+    
+'''
+try:
+    n1=int(input())
+    n2=int(input())
+    n3=n1/n2
+    print(n3)
+except ZeroDivisionError:
+    print("It can't be divided by zero")  
+else:
+    print("all done")
+finally:
+    print("clean up activity")
+    
+print("End of application")
