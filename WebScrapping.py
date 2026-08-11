04cd1d6f0e8f827cdc21dc3ca2bf0754850fd757
Honde Kajal Santosh <hondekajal@gmail.com>
2026-08-11 22:28:27 +0530

    WebScrapping

--- WebScrapping.py
+++ WebScrapping.py
@@ -1,4 +1,5 @@
 # Process of collecting information from web pages
+'''
 import re
 import urllib, urllib.request
 
@@ -7,3 +8,14 @@ u=urllib.request.urlopen(s)
 data=u.read()
 l=re.findall("<title>.*</title>",str(data),re.IGNORECASE)
 print(l)
+'''
+
+import re , urllib
+import urllib.request
+sites=["google","rediff"]
+for s in sites:
+    print("searching...",s)
+    u=urllib.request.urlopen("http://"+s+".com")
+    text=u.read()
+    title=re.findall("<title>.*</title>",str(text),re.IGNORECASE)
+    print(title[0])
